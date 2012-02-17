fbecf2caf495ee6431cca2c886136d87cb062b94
Ethan White <ethan@weecology.org>
2012-02-17 22:14:49 +0000

    Refactor mete_geog.py into functions to allow for easy use across datasets (and to avoid general embarrassment

--- mete_geog.py
+++ mete_geog.py
@@ -21,35 +21,44 @@ import matplotlib.pylab as plt
 from mete import get_beta, get_mete_rad
 from macroeco import plot_color_by_pt_dens
 
-bbs_envpred_data = read_csv('data/bbs_out.csv')
-bbs_sad_data = read_csv('../data/bbs_obs_pred.csv', names=['SiteID',
+def import_data(datadir, dataset):
+    envpred_data = read_csv(datadir + dataset + '_out.csv')
+    sad_data = read_csv(datadir + dataset +'_obs_pred.csv', names=['SiteID',
                                                                   'Year',
                                                                   'Obs',
                                                                   'Pred'])
-#Deal with the fact that there are different sites in bbs_out.csv and
-#bbs_obs_pred.csv
-bbs_sad_data = bbs_sad_data.ix[np.in1d(bbs_sad_data['SiteID'].values,
-                                       bbs_envpred_data['SiteID'].values)]
+    #Deal with the fact that in some cases there are different sites in
+    #*_out.csv and *_obs_pred.csv
+    sad_data = sad_data.ix[np.in1d(sad_data['SiteID'].values,
+                                   envpred_data['SiteID'].values)]
+    return envpred_data, sad_data
 
-envpred_sads = DataFrame(columns=['SiteID', 'EnvPred'])
-for index, site in bbs_envpred_data.iterrows():
-    obs_S = site['S']
-    envpred_S = 10 ** site['predlogS']
-    envpred_N = 10 ** site['predlogN']
-    beta = get_beta(envpred_S, envpred_N)
-    #To produce a comparable number of species use obs_S; IS THIS RIGHT?
-    site_sad, p = get_mete_rad(obs_S, envpred_N, beta=beta)
-    site_sad_with_id = DataFrame(np.column_stack([site['SiteID'] *
-                                                  np.ones(len(site_sad)),
-                                                  site_sad]),
-                                 columns=['SiteID', 'EnvPred'])
-    envpred_sads = envpred_sads.append(site_sad_with_id, ignore_index=True)
+def get_envpred_sads(envpred_data):
+    envpred_sads = DataFrame(columns=['SiteID', 'EnvPred'])
+    for index, site in envpred_data.iterrows():
+        obs_S = site['S']
+        envpred_S = 10 ** site['predlogS']
+        envpred_N = 10 ** site['predlogN']
+        beta = get_beta(envpred_S, envpred_N)
+        #To produce a comparable number of species use obs_S; IS THIS RIGHT?
+        site_sad, p = get_mete_rad(obs_S, envpred_N, beta=beta)
+        site_sad_with_id = DataFrame(np.column_stack([site['SiteID'] *
+                                                      np.ones(len(site_sad)),
+                                                      site_sad]),
+                                     columns=['SiteID', 'EnvPred'])
+        envpred_sads = envpred_sads.append(site_sad_with_id, ignore_index=True)
+    return envpred_sads
 
-plt.subplot(1, 2, 1)
-plt.loglog(bbs_sad_data['Pred'], bbs_sad_data['Obs'], 'bo')
-plt.loglog([min(bbs_sad_data['Pred']), max(bbs_sad_data['Pred'])], 
-         [min(bbs_sad_data['Pred']), max(bbs_sad_data['Pred'])], 'k-')
-plt.subplot(1, 2, 2)
-plt.loglog(envpred_sads['EnvPred'], bbs_sad_data['Obs'], 'ro')
-plt.loglog([min(envpred_sads['EnvPred']), max(envpred_sads['EnvPred'])], 
-         [min(envpred_sads['EnvPred']), max(envpred_sads['EnvPred'])], 'k-')
+def plot_obs_pred(sad_data, envpred_sads):
+    plt.subplot(1, 2, 1)
+    plt.loglog(sad_data['Pred'], sad_data['Obs'], 'bo')
+    plt.loglog([min(sad_data['Pred']), max(sad_data['Pred'])], 
+               [min(sad_data['Pred']), max(sad_data['Pred'])], 'k-')
+    plt.subplot(1, 2, 2)
+    plt.loglog(envpred_sads['EnvPred'], sad_data['Obs'], 'ro')
+    plt.loglog([min(envpred_sads['EnvPred']), max(envpred_sads['EnvPred'])], 
+               [min(envpred_sads['EnvPred']), max(envpred_sads['EnvPred'])], 'k-')
+    
+envpred_data, sad_data = import_data('./data/', 'bbs')
+envpred_sads = get_envpred_sads(envpred_data)
+plot_obs_pred(sad_data, envpred_sads)
