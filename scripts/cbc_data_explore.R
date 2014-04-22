83eb91347deed45f3849c1f05d4ce00b487cfe91
Daniel McGlinn <danmcglinn@gmail.com>
2014-04-22 10:10:14 -0600

    Add mapping of the CBC data, correct geographic subsetting, and extract site coordinates

--- scripts/cbc_data_explore.R
+++ scripts/cbc_data_explore.R
@@ -1,4 +1,19 @@
-# CBC
+library(maps)
+
+spab = read.csv('./data/raw/cbc_spab.csv')
+coords = read.csv('./data/raw/cbc_coords.csv')
+true = coords$loc_id %in% spab$loc_id
+coords = coords[true, ]
+
+pdf('./figs/cbc_map.pdf')
+map('world', c('canada', 'usa','mexico'),
+    xlim=c(-180, -50), ylim=c(15, 70))
+map('state', add=T)
+points(coords$longitude, coords$latitude, cex=.25,
+       pch=19, col='red')
+dev.off()
+
+# CBC MySQL queries -----------------------------------------------------------
 # Step 1. Group By SPECIES_CODE and by TOO WHERE DIURNAL LANDBIRD = 1 - 
 # to yield sp_too
 DROP DATABASE IF EXISTS queries;
@@ -28,7 +43,7 @@ FROM CBC.SUB_AUX INNER JOIN CBC.OBS ON
 SUB_AUX.SUB_ID = OBS.SUB_ID
 WHERE SUB_AUX.COUNT_YR = 109;
 
-# Step 3. LINK obs_1 to SUB by SUB_ID to add LOC_ID 
+# Step 3. LINK obs_1 to SUB by SUB_ID to add LOC_ID
 
 CREATE TABLE queries.obs_2
 SELECT SUB.LOC_ID, obs_1.* 
@@ -36,14 +51,16 @@ FROM queries.obs_1 INNER JOIN CBC.SUB ON
 obs_1.SUB_ID = SUB.SUB_ID;
 
 # Step 4. LINK obs_2 to LOC by LOC_ID to remove records WHERE SUBNATIONAL1_CODE 
-# IS NOT US-HI AND COUNTRY CODE IS 'CA', 'US', OR 'US-CA'
+# IS US-HI AND COUNTRY CODE IS NOT 'CA', 'US', OR 'US-CA'
 
 CREATE TABLE queries.obs_3
 SELECT obs_2.* 
-    FROM queries.obs_2 INNER JOIN CBC.LOC ON obs_2.LOC_ID = LOC.LOC_ID
-WHERE LOC.COUNTRY_CODE = 'CA' OR LOC.COUNTRY_CODE = 'US' 
-OR LOC.COUNTRY_CODE = 'US-CA' AND 
-LOC.SUBNATIONAL1_CODE != 'US-HI';
+FROM queries.obs_2 INNER JOIN CBC.LOC ON
+obs_2.LOC_ID = LOC.LOC_ID
+WHERE LOC.COUNTRY_CODE IN ('CA', 'US', 'US-CA') AND 
+LOC.SUBNATIONAL1_CODE NOT IN ('US-HI', 'US-AK')
+
+## what about alaska here (drop AK), 
 
 # Step 5. CREATE obs_4 by adding sp_too.TOO
 
@@ -52,24 +69,43 @@ SELECT obs_3.*, sp_too.TOO
 FROM queries.obs_3 INNER JOIN queries.sp_too 
 ON obs_3.SPECIES_CODE = sp_too.SPCODE;
 
-# Step 6. To create table with SiteID - Year - Sp - abund:
+# Step 6. To create table with SiteID - Year - Duration_hrs - Num_Obs - Sp - abund:
 # GROUPing BY LOC_ID and SPECIES_CODE and SUMmming over HOW_MANY FROM obs_4
 
 CREATE TABLE queries.obs_5                
-SELECT obs_4.LOC_ID, obs_4.COUNT_YR AS YEAR, obs_4.TOO, 
-SUM(obs_4.HOW_MANY) AS AB
+SELECT obs_4.LOC_ID, obs_4.COUNT_YR AS YEAR, obs_4.TOO, SUM(obs_4.HOW_MANY) AS AB
 FROM queries.obs_4 
 GROUP BY obs_4.LOC_ID, obs_4.COUNT_YR, obs_4.TOO;
 
 # Step 7. Remove zeroes from data, and save table to file
 
+SELECT 'loc_id', 'year', 'too', 'ab'
+UNION ALL 
 SELECT * FROM queries.obs_5 
 WHERE obs_5.AB > 0
 INTO OUTFILE '/tmp/cbc_spab.csv'
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n';
 
-## export all the datafiles
+# Step 8. Export coordinates and measures of sampling effort.
+
+CREATE TABLE queries.coords
+SELECT LOC.LOC_ID, SUB.SUB_ID, LOC.LATITUDE, LOC.LONGITUDE, SUB.DURATION_HRS
+FROM CBC.LOC
+INNER JOIN CBC.SUB 
+      ON LOC.LOC_ID = SUB.LOC_ID ;
+
+SELECT 'loc_id', 'sub_id', 'latitude', 'longitude', 'duration_hrs', 'num_observers'
+UNION ALL 
+SELECT coords.*, SUB_AUX.NUM_OBSERVERS
+FROM queries.coords INNER JOIN CBC.SUB_AUX ON
+coords.SUB_ID = SUB_AUX.SUB_ID
+INTO OUTFILE '/tmp/cbc_coords.csv'
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n';
+
+
+## examine and export  datafiles ----------------------------------------------
 SHOW TABLES FROM CBC ;
 show columns from CBC.AUX_LOC ;
 show columns from CBC.AUX_WEATHER ;
