5520cb2c6ee79c91849abe2a69058ed780f6e971
Daniel McGlinn <danmcglinn@gmail.com>
2014-07-17 13:02:01 -0600

    Corrected mistakes related to scaling and variable handling

--- scripts/state_var_plots.py
+++ scripts/state_var_plots.py
@@ -13,8 +13,8 @@ from macroecotools import plot_color_by_pt_dens, obs_pred_rsquare
 def plot_obs_pred(obs_pred_data, yvar, dest_file='./obs_pred.png'):
     plot_color_by_pt_dens(10**obs_pred_data[yvar + 'pred'], 10**obs_pred_data[yvar],
                           3, loglog=1)
-    plt.loglog([min(10**obs_pred_data['pred'] ), max(10**obs_pred_data['pred'] )], 
-               [min(10**obs_pred_data['pred'] ), max(10**obs_pred_data['pred'] )], 'k-')
+    plt.loglog([min(10**obs_pred_data[yvar + 'pred']), max(10**obs_pred_data[yvar])], 
+               [min(10**obs_pred_data[yvar + 'pred']), max(10**obs_pred_data[yvar])], 'k-')
     plt.savefig(dest_file, dpi = 400)
 
 if len(sys.argv) > 1:
@@ -22,13 +22,13 @@ if len(sys.argv) > 1:
 else:
     datasets = ['bbs_2012', 'bbs_2008_2012', 'cbc', 'fia', 'gentry', 'naba']
 
-yvars = ['lsr', 'lab']
+yvars = ['logS']
 
 for dataset in datasets:
     for datatype in ['fit', 'test']:
         obs_pred_data = read_csv('./results/' + dataset + '_state_var_' + datatype + '_obs_pred.csv')
         for yvar in yvars:
-            fig_name = './figs/' + dataset + '_sate_var_' + datatype + '_obs_pred.png'
-            plot_obs_pred(obs_pred_data, adj=adj, dest_file=fig_name)
+            fig_name = './figs/' + dataset + '_' + yvar + '_' + datatype + '_obs_pred.png'
+            plot_obs_pred(obs_pred_data, yvar, dest_file=fig_name)
 
 
