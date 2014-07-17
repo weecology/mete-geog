f89b5cc07978a2881d1c3262886507173ad3b1e0
Dan McGlinn <danmcglinn@gmail.com>
2014-07-16 23:48:40 -0400

    Add computation and export of rad in addition to sad

--- scripts/mete_geog.py
+++ scripts/mete_geog.py
@@ -18,12 +18,8 @@ from math import log, ceil
 import numpy as np
 import sys
 
-from mete import get_mete_sad, which
+from mete import get_mete_rad, get_mete_sad, which
 from macroecotools import plot_color_by_pt_dens, obs_pred_rsquare
-
-def import_data(resultdir, rawdir, datatype, dataset):
-    envpred_data = read_csv(resultdir + dataset + '_state_var_' + datatype + '_obs_pred.csv')
-    return envpred_data, sad_data
 
 def get_log_bins(abu, logbase=2):
     max_bin = log(max(abu), logbase)
@@ -34,21 +30,31 @@ def get_log_bins(abu, logbase=2):
     bins = [logbase ** x for x in range(0, max_bin + 1)]
     return bins
 
-def get_envpred_sads(envpred_data):
-    envpred_sads = DataFrame(columns=['site_id', 'octave', 'env_pred'])
+def get_envpred(envpred_data, predtype=['sad', 'rad']):
+    if predtype is 'sad':
+        envpred = DataFrame(columns=['site_id', 'octave', 'env_pred'])
+    if predtype is 'rad':
+        envpred = DataFrame(columns=['site_id', 'rank', 'env_pred'])
     for index, site in envpred_data.iterrows():
         obs_S = site['S']
         envpred_S = 10 ** site['logSpred']
         envpred_N = 10 ** site['logNpred']
-        #To produce a comparable number of species use obs_S; IS THIS RIGHT?
-        sad_bins = get_log_bins([envpred_N])
-        octave = range(0, len(sad_bins) - 1)
-        site_sad = get_mete_sad(envpred_S, envpred_N, bin_edges=sad_bins)
-        site_ids = [site['site_id'] for i in range(0, len(site_sad))]
-        site_sad_with_id = DataFrame(np.column_stack([site_ids, octave, site_sad]),
-                                     columns=['site_id', 'octave', 'env_pred'])
-        envpred_sads = envpred_sads.append(site_sad_with_id, ignore_index=True)
-    return envpred_sads
+        if predtype is 'sad':        
+            sad_bins = get_log_bins([envpred_N])
+            octave = range(0, len(sad_bins) - 1)
+            site_pred = get_mete_sad(envpred_S, envpred_N, bin_edges=sad_bins)
+            site_ids = [site['site_id'] for i in range(0, len(site_pred))]
+            site_pred_with_id = DataFrame(np.column_stack([site_ids, octave, site_pred]),
+                                          columns=['site_id', 'octave', 'env_pred'])    
+        if predtype is 'rad':
+            # note using observed S here for time being            
+            rank = range(1, int(obs_S + 1))
+            site_pred, p = get_mete_rad(obs_S, envpred_N)
+            site_ids = [site['site_id'] for i in range(0, len(site_pred))]
+            site_pred_with_id = DataFrame(np.column_stack([site_ids, rank, site_pred]),
+                                          columns=['site_id', 'rank', 'env_pred'])
+        envpred = envpred.append(site_pred_with_id, ignore_index=True)
+    return envpred
 
 if len(sys.argv) > 1:
     datasets = [sys.argv[1]]
@@ -57,11 +63,12 @@ else:
 
 for dataset in datasets:
     for datatype in ['fit', 'test']:
-        envpred_data = read_csv('./results/' + dataset + '_state_var_' + datatype + '_obs_pred.csv')
-        # check that predicted S smaller than predicted N
-        rows_to_keep = (envpred_data.logNpred - envpred_data.logSpred) > 0
-        envpred_data = envpred_data[rows_to_keep]
-        envpred_sads = get_envpred_sads(envpred_data)
-        envpred_sads.to_csv('./results/' + dataset + '_sad_' + datatype + '_pred.csv')
-        print dataset + ' ' + datatype + ' complete!' 
+        for predtype in ['rad']:
+            envpred_data = read_csv('./results/' + dataset + '_state_var_' + datatype + '_obs_pred.csv')
+            # check that predicted S smaller than predicted N
+            rows_to_keep = (envpred_data.logNpred - envpred_data.logSpred) > 0
+            envpred_data = envpred_data[rows_to_keep]
+            envpred = get_envpred(envpred_data, predtype)
+            envpred.to_csv('./results/' + dataset + '_', predtype + '_' + datatype + '_pred.csv')
+            print dataset + ' ' + datatype + ' ' + predtype + ' ' + 'complete!'
 
