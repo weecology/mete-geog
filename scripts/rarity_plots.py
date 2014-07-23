35fa9545422ae83bb6a082d22765810b4546e8f8
Dan McGlinn <danmcglinn@gmail.com>
2014-07-23 16:58:57 -0400

    Fix how rarity dataframe was used in plotting

--- scripts/rarity_plots.py
+++ scripts/rarity_plots.py
@@ -11,14 +11,7 @@ import sys
 from macroecotools import plot_color_by_pt_dens, obs_pred_rsquare
 
 def plot_obs_pred(obs_pred_data, dest_file='./obs_pred.png'):
-    sites = list(set(obs_pred_data['site_id'].values))
-    obs_tot = []
-    pred_tot = []
-    for i in sites:
-        tmp = obs_pred_data[obs_pred_data['site_id'] == i]
-        obs_tot.append(sum(tmp['obs'].values))
-        pred_tot.append(sum(tmp['pred'].values))    
-    plot_color_by_pt_dens(pred_tot, obs_tot, 3, loglog=1)
+    plot_color_by_pt_dens(obs_pred_data['pred'], obs_pred_data['obs'], 3, loglog=1)
     plt.loglog([min(pred_tot), max(pred_tot)], 
                [min(pred_tot), max(pred_tot)], 'k-')
     plt.savefig(dest_file, dpi = 400)    
@@ -30,17 +23,9 @@ else:
 
 for dataset in datasets:
     for datatype in ['fit', 'test']:
-        for predtype in ['sad']:
+        for predtype in ['rare']:
             obs_pred_data = read_csv('./results/' + dataset + '_' + predtype+ '_' + datatype + '_obs_pred_norm.csv')
-            obs_pred_data = obs_pred_data[obs_pred_data['octave'] <= 4]
-            sites = list(set(obs_pred_data['site_id'].values))
-            obs_tot = []
-            pred_tot = []
-            for i in sites:
-                tmp = obs_pred_data[obs_pred_data['site_id'] == i]
-                obs_tot.append(sum(tmp['obs'].values))
-                pred_tot.append(sum(tmp['pred'].values))
-            print obs_pred_rsquare(np.array(pred_tot), np.array(obs_tot))
+            print obs_pred_rsquare(np.array(obs_pred_data['obs'].values), np.array(obs_pred_data['pred'].values))
             fig_name = './figs/' + dataset + '_' + datatype +'_obs_pred_rarity.png'
             plot_obs_pred(obs_pred_data, dest_file=fig_name)
 
