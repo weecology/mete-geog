81ef73d5d0e0c710a53637a774c3c3f83362764f
Dan McGlinn <danmcglinn@gmail.com>
2014-07-23 15:51:08 -0400

    Fix the plot_obs_pred function so that it property handles the rarity bins

--- scripts/rarity_plots.py
+++ scripts/rarity_plots.py
@@ -11,11 +11,17 @@ import sys
 from macroecotools import plot_color_by_pt_dens, obs_pred_rsquare
 
 def plot_obs_pred(obs_pred_data, adj=0, dest_file='./obs_pred.png'):
-    plot_color_by_pt_dens(obs_pred_data['pred'] + adj, obs_pred_data['obs'] + adj,
-                          3, loglog=1)
-    plt.loglog([min(obs_pred_data['pred'] + adj), max(obs_pred_data['pred'] + adj)], 
-               [min(obs_pred_data['pred'] + adj), max(obs_pred_data['pred'] + adj)], 'k-')
-    plt.savefig(dest_file, dpi = 400)
+    sites = list(set(obs_pred_data['site_id'].values))
+    obs_tot = []
+    pred_tot = []
+    for i in sites:
+        tmp = obs_pred_data[obs_pred_data['site_id'] == i]
+        obs_tot.append(sum(tmp['obs'].values))
+        pred_tot.append(sum(tmp['pred'].values))    
+    plot_color_by_pt_dens(pred_tot + adj, obs_tot + adj, 3, loglog=1)
+    plt.loglog([min(pred_tot + adj), max(pred_tot + adj)], 
+               [min(pred_tot + adj), max(pred_tot + adj)], 'k-')
+    plt.savefig(dest_file, dpi = 400)    
 
 if len(sys.argv) > 1:
     datasets = [sys.argv[1]]
@@ -35,9 +41,7 @@ for dataset in datasets:
                 obs_tot.append(sum(tmp['obs'].values))
                 pred_tot.append(sum(tmp['pred'].values))
             adj = 0
-            log_pred = [log(i + adj) for i in pred_tot]
-            log_obs = [log(i + adj) for i in obs_tot]
-            print obs_pred_rsquare(np.array(log_pred), np.array(log_obs))
+            print obs_pred_rsquare(np.array(pred_tot), np.array(obs_tot))
             fig_name = './figs/' + dataset + '_' + datatype +'_obs_pred_rarity.png'
             plot_obs_pred(obs_pred_data, adj=adj, dest_file=fig_name)
 
